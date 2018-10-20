092b7afcfb4f848a2ae74fd98cf42ced9be972de
XTidusX <krzuchowsk@wp.pl>
2018-10-20 14:51:50 +0200

    user class

---
+++ src/com/sda/git/User.java
@@ -0,0 +1,12 @@
+package com.sda.git;
+
+public class User {
+    private  String name;
+    private  String lastname;
+    private  int age;
+
+    public User(String name,String lastname, int age){
+        
+    }
+}
+
